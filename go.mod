fba8095829bf7991da4cb4fcf30d10d152d8ba3a
morning-start <morning-start@foxmail.com>
2025-07-07 23:07:01 +0800

    feat: 添加git标签管理功能
    实现git标签的创建、版本号递增和推送功能：
    - 添加semver依赖用于版本号管理
    - 支持列出所有标签(ls命令)
    - 支持递增patch/minor/major版本号
    - 支持推送标签到远程仓库
    - 添加帮助信息显示

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module git-tags
 
 go 1.24.4
+
+require github.com/Masterminds/semver/v3 v3.3.1
--- main.go
+++ main.go
@@ -1,7 +1,112 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/Masterminds/semver/v3"
+)
+
+const initialVersion = "0.0.0"
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) < 2 {
+		showHelp()
+		return
+	}
+
+	cmd := os.Args[1]
+
+	switch cmd {
+	case "ls":
+		listTags()
+	case "patch":
+		bumpVersion("patch")
+	case "minor":
+		bumpVersion("minor")
+	case "major":
+		bumpVersion("major")
+	case "push":
+		pushTags()
+	case "--help", "-h":
+		showHelp()
+	default:
+		showHelp()
+	}
+}
+
+func listTags() {
+	cmd := exec.Command("git", "tag")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error listing tags: %v\n", err)
+		return
+	}
+	fmt.Println(string(output))
+}
+
+func getLatestTag() string {
+	cmd := exec.Command("git", "tag", "--sort=-v:refname")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return initialVersion
+	}
+	tags := strings.Split(strings.TrimSpace(string(output)), "\n")
+	if len(tags) > 0 && tags[0] != "" {
+		return tags[0]
+	}
+	return initialVersion
+}
+
+func bumpVersion(level string) {
+	latestTag := getLatestTag()
+	v, err := semver.NewVersion(latestTag)
+	if err != nil {
+		v, _ = semver.NewVersion(initialVersion)
+	}
+
+	var newVersion semver.Version
+	switch level {
+	case "patch":
+		newVersion = v.IncPatch()
+	case "minor":
+		newVersion = v.IncMinor()
+	case "major":
+		newVersion = v.IncMajor()
+	}
+
+	newTag := newVersion.String()
+	cmd := exec.Command("git", "tag", newTag)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating tag: %v\n", err)
+		fmt.Println(string(output))
+		return
+	}
+	fmt.Printf("Created tag %s\n", newTag)
+}
+
+func pushTags() {
+	cmd := exec.Command("git", "push", "--tags")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error pushing tags: %v\n", err)
+		fmt.Println(string(output))
+		return
+	}
+	fmt.Println(string(output))
+}
+
+func showHelp() {
+	fmt.Println(`Usage: git-tags [command]
+
+Commands:
+  ls		Show all tags
+  patch		Increment patch version
+  minor		Increment minor version
+  major		Increment major version
+  push		Push tags to remote
+  --help, -h	Show this help message`)
 }
