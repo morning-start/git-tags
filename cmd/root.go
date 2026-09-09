8c6e7fd0a6db60ddfd3927ac259fc558ef0b3991
morning-start <morning-start@foxmail.com>
2026-09-09 14:53:19 +0800

    chore(release): bump to v2.3.0

--- cmd/root.go
+++ cmd/root.go
@@ -129,7 +129,7 @@ var RootCmd = &cobra.Command{
 	Short:             "Manage git tags",
 	Long:              "A tool to manage git tags with version bumping capabilities.",
 	CompletionOptions: cobra.CompletionOptions{DisableDefaultCmd: true},
-	Version:           "2.2.4",
+	Version:           "2.3.0",
 }
 
 var ListCmd = &cobra.Command{
